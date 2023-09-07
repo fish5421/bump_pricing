7b2cb2342711422d31664a73b69fb076c9e56f82
Peter Correia <petercorreia2013@gmail.com>
2023-09-07 19:30:51 -0400

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Account from './Account';
 import SuccessPage from './SuccessPage';
 import CancelPage from './CancelPage';
 import 'antd/dist/reset.css';
+import { Menu } from 'antd'; // Importing Ant Design's Menu component
+
 
 function App() {
   const [session, setSession] = useState(null);
@@ -22,12 +24,19 @@ function App() {
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div className="flex justify-center items-center h-screen w-screen bg-gray-100">
-        <Routes>
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/cancel" element={<CancelPage />} />
-          <Route path="/" element={!session ? <Auth /> : <Account key={session.user.id} session={session} />} />
-        </Routes>
+      <div className="flex flex-col h-screen w-screen bg-gray-100">
+        <Menu mode="horizontal">
+          <Menu.Item key="home"><Link to="/">Home</Link></Menu.Item>
+          <Menu.Item key="success"><Link to="/success">Success</Link></Menu.Item>
+          <Menu.Item key="cancel"><Link to="/cancel">Cancel</Link></Menu.Item>
+        </Menu>
+        <div className="flex-grow flex justify-center items-center">
+          <Routes>
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/cancel" element={<CancelPage />} />
+            <Route path="/" element={!session ? <Auth /> : <Account key={session.user.id} session={session} />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
