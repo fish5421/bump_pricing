072116841004c9a7337d5939803c77bc7608ac44
Peter Correia <petercorreia2013@gmail.com>
2023-09-07 20:08:28 -0400

    flow

--- src/Account.jsx
+++ src/Account.jsx
@@ -108,50 +108,49 @@ export default function Account({ session }) {
             <SubscriptionPage session={session} />
         ) : (
             <>
-                <div className="bg-white text-center font-bold text-2xl mb-4">Darrel's Community</div>
-                <StepWizard>
-                    <Step1 session={session} username={username} setUsername={setUsername} />
-                    <Step2 session={session} businessName={businessName} setBusinessName={setBusinessName} />
-                    <Step3 session={session} updateProfile={updateProfile} loading={loading} />
-                    <Step4 session={session} isSubscribed={isSubscribed} handleSubscriptionClick={handleSubscriptionClick} />
-                </StepWizard>
+                <header className="bg-blue-500 text-white text-center py-4">
+                    Darrel's Community
+                </header>
+                <form onSubmit={updateProfile} className="flex flex-col items-center justify-center h-screen w-full bg-gray-100 overflow-hidden">
+                    <div className="bg-white p-4 sm:p-6 md:p-8 lg:p-10 rounded-lg shadow-lg w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
+                        {/* Email */}
+                        <div className="mb-4 w-full text-black">
+                            <label htmlFor="email" className="text-lg font-medium text-gray-600">
+                                Email <Tooltip title="We need your email to send you updates and account-related information.">❗️</Tooltip>
+                            </label>
+                            <input id="email" type="text" value={session.user.email} disabled className="w-full flowbite-input" />
+                        </div>
+                        {/* Username */}
+                        <div className="mb-4 w-full text-black">
+                            <label htmlFor="username" className="text-lg font-medium text-gray-600">
+                                Name <Tooltip title="Your name helps personalize your experience.">❗️</Tooltip>
+                            </label>
+                            <input id="username" type="text" required value={username || ''} onChange={(e) => setUsername(e.target.value)} className="w-full flowbite-input" />
+                        </div>
+                        {/* Business Name */}
+                        <div className="mb-4 w-full text-black">
+                            <label htmlFor="businessName" className="text-lg font-medium text-gray-600">
+                                Business Name <Tooltip title="Optional. Add your business name for business-related services.">❗️</Tooltip>
+                            </label>
+                            <input id="businessName" type="text" value={businessName || ''} onChange={(e) => setBusinessName(e.target.value)} className="w-full flowbite-input" />
+                        </div>
+                        {/* Action buttons */}
+                        <div className="mb-4 w-full flex flex-col sm:flex-row sm:justify-between gap-9">
+                            <button type="submit" disabled={loading} className="mb-2 sm:mb-0 w-full sm:w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg p-2 sm:p-3 md:p-4 lg:p-5">
+                                {loading ? 'Loading ...' : 'Update'}
+                            </button>
+                            <button type="button" onClick={() => supabase.auth.signOut()} className="w-full sm:w-1/2 text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-lg p-2 sm:p-3 md:p-4 lg:p-5">
+                                Sign Out
+                            </button>
+                        </div>
+                        <div className="mt-6 w-full">
+                            <button type="button" className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg p-2 sm:p-3 md:p-4 lg:p-5" onClick={handleSubscriptionClick}>
+                                {isSubscribed ? 'Cancel Subscription' : 'Subscribe Now'}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </>
         )
     );
-}
-
-// Separate the forms into smaller components, one for each "step" in the user onboarding flow
-const Step1 = ({ session, username, setUsername }) => {
-    return (
-        <div className="mb-4 w-full text-black">
-            <label htmlFor="email" className="text-lg font-medium text-gray-600">Email</label>
-            <input id="email" type="text" value={session.user.email} disabled className="w-full flowbite-input" />
-        </div>
-    );
-};
-
-const Step2 = ({ session, businessName, setBusinessName }) => {
-    return (
-        <div className="mb-4 w-full text-black">
-            <label htmlFor="businessName" className="text-lg font-medium text-gray-600">Business Name</label>
-            <input id="businessName" type="text" value={businessName || ''} onChange={(e) => setBusinessName(e.target.value)} className="w-full flowbite-input" />
-        </div>
-    );
-};
-
-const Step3 = ({ session, updateProfile, loading }) => {
-    return (
-        <button type="submit" disabled={loading} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg p-2 sm:p-3 md:p-4 lg:p-5">
-            {loading ? 'Loading ...' : 'Update'}
-        </button>
-    );
-};
-
-const Step4 = ({ session, isSubscribed, handleSubscriptionClick }) => {
-    return (
-        <button type="button" className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg p-2 sm:p-3 md:p-4 lg:p-5" onClick={handleSubscriptionClick}>
-            {isSubscribed ? 'Cancel Subscription' : 'Subscribe Now'}
-        </button>
-    );
-
 };
